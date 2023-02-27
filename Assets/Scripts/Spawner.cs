89ee28990896a15cbaf936ef02dffc34ab1c8afd
LauraHNunez <110181542+LauraHNunez@users.noreply.github.com>
2023-02-27 19:43:17 +0100

    First commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] cubes;
+	public Transform[] spawnPos;
+	private float timer;
+	public float beat = (60/145)*2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > beat){
+			int cubeColor = Random.Range(0,3);
+			GameObject cube = Instantiate(cubes[cubeColor], spawnPos[Random.Range(0,6)]);
+			cube.transform.localPosition = Vector3.zero;
+//			cube.transform.Rotate(transform.forward, 90 * Random.Range(0,4));
+			if(cubeColor == 2) {
+				timer -= beat*2;
+			}else{
+				timer -= beat;
+			}
+		}
+		timer += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/cubeCollision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "red") {
+			Destroy(col.gameObject);
+		}
+		if(col.gameObject.tag == "yellow") {
+			Debug.Log("y: "+col.gameObject.transform.position);
+			col.gameObject.transform.position = new Vector3(0,col.gameObject.transform.position.y, col.gameObject.transform.position.z +2); 
+			int flag = Random.Range(0,2);
+			if(flag < 1) {
+				col.gameObject.transform.Rotate(0.0f, 25.0f*-1, 0.0f);
+			}else {
+				col.gameObject.transform.Rotate(0.0f, 25.0f, 0.0f);
+			}
+
+			if (col.gameObject.transform.rotation.y != 0) {
+				Destroy(col.gameObject);
+			}
+			Debug.Log("Yellow "+col.gameObject.transform.rotation.y);
+			StartCoroutine(WaitForFunction());
+//			Debug.Log("ysadsadasdas "+col.gameObject.transform.position);
+		}
+		if(col.gameObject.tag == "black") {
+		
+		}
+	} 
+	IEnumerator WaitForFunction()
+	{
+		yield return new WaitForSeconds(3);
+		Debug.Log("Hello!");  
+	}
+
+}
